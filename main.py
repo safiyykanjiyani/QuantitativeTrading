9b3194790eeeaa0f647e78030847aa68c3f2c199
Safiyy Kanjiyani <safiyykanjiyani@gmail.com>
2023-07-12 01:14:17 +0000

    Added code

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from alpaca.data.historical import StockHistoricalDataClient
 from alpaca.data.requests import StockBarsRequest
 from alpaca.data.timeframe import TimeFrame
 from datetime import datetime
-import requests
 import toml
 import csv
 import finnhub
+import os
+import time
 
 
 #import api and secret key from config file
@@ -18,7 +19,7 @@ key_api = config['finnhub']['api_key']
 companies = {}
 
 #import ticker csv file
-with open('BAD-holdings.csv') as csvfile:
+with open('DWAS-holdings.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for line_number, row in enumerate(readCSV):
         if line_number != 0:
@@ -27,16 +28,21 @@ with open('BAD-holdings.csv') as csvfile:
 #initialize client
 client = StockHistoricalDataClient(api_key, secret_key)
 
+year = 2023
+
+
 
 for company, ticker in companies.items():
     #go through tickers of companies
     symbol = ticker
 
+    '''
+
     parameters = StockBarsRequest(
         symbol_or_symbols = symbol,
-        timeframe = TimeFrame.Day,
-        start = datetime(2022, 12, 31),
-        end = datetime(2023, 1, 7)
+        timeframe = TimeFrame.Month,
+        start = datetime(year, 12, 31),
+        end = datetime(year + 1, 6, 30)
     )
 
     try: 
@@ -45,9 +51,32 @@ for company, ticker in companies.items():
     except:
         continue
 
+    '''
+
     #lookup lobbying expenditures for company
     finnhub_client = finnhub.Client(api_key=key_api)
+    time.sleep(1)
 
-    print(finnhub_client.stock_lobbying(symbol, "2021-01-01", "2022-06-15"))
+    try:
+        lobbying_data = finnhub_client.stock_lobbying(symbol, f"2022-6-30", f"2023-4-30")
+        #for every stock, create a csv file with the stock data
+    except:
+        pass
+    
+    print(lobbying_data)
+    print(lobbying_data['data'] == [])
+
+    if lobbying_data['data'] != []:
+        keys = lobbying_data["data"][0].keys()
+        with open(f"lobbying/{symbol}.csv", "w") as f:
+            writer = csv.DictWriter(f, fieldnames=keys)
+            writer.writeheader()
+            writer.writerows(lobbying_data["data"])
 
 
+    
+
+#iterate through folder, and for each file, if it is empty, delete it
+for filename in os.listdir('lobbying'):
+    if os.stat(f"lobbying/{filename}").st_size == 0:
+        os.remove(f"lobbying/{filename}")
